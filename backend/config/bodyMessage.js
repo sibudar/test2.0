ee54a240af42056047fec85eb6e3a57282f70e91
Gift <gift@thedigitalacademy.co.za>
2020-02-21 10:42:52 +0200

    Added sendmail feature to registration route, added subject to sendMail function to reset-password rout.

--- backend/config/bodyMessage.js
+++ backend/config/bodyMessage.js
@@ -1,13 +1,24 @@
-
 // <h2>Here is your password reset key</h2>
 // <h2><code contenteditable="false" style="font-weight:200;font-size:1.5rem;padding:5px 10px; background: #EEEEEE; border:0">$</code></h4>
 // <p>Please ignore if you didn't try to reset your password on our platform</p>`
 
-function resetMessage(name, link){
-    return `<h2>Hi ${name}</h2>
-    <h2>Please click <a href="${link}">here</a> to reset your password </h2>
-    <h2><code contenteditable="false" style="font-weight:200;font-size:1.5rem;padding:5px 10px; background: #EEEEEE; border:0">$</code></h4>
+function resetMessage (name, link) {
+  return `<h2>Hi ${name}</h2>
+    <p>Please click <a href="${link}">here</a> to reset your password </p>
     <p>Please ignore if you didn't try to reset your password on our platform</p>`;
 }
 
-module.exports = {resetMessage}
+function registerMessage (name) {
+  return `<h2>Hi ${name}</h2>
+    <p>Congratulation! Your account has been registered successful</p>`;
+}
+
+function resetSubject () {
+  return `<h2>Novelty | Reset Password</h2>`;
+}
+
+function registerSubject () {
+  return `<h2>Novelty | Account Registered</h2>`;
+}
+
+module.exports = {resetMessage, registerMessage, resetSubject, registerSubject};
