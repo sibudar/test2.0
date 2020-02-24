c135b19b2d56f22bbdcfe89e6f09e01561eadde0
pmahele <philani@thedigitalacademy.co.za>
2020-02-24 08:34:51 +0200

    fixing database characters

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-const fieldValidator = require ('../Helpers/validate');
-const fieldResponse = require ('../Helpers/httpResponse');
-=======
 const fieldValidator = require('../Helpers/validate');
 const fieldResponse = require('../Helpers/httpResponse');
->>>>>>> 9d7de7dfc5e104ef1fbd037645a1c1c00b421b24
 const queryResponse = require('../Helpers/queryFunction');
 const bcrypt = require('bcryptjs');
 const mail = require('../Helpers/sendEmail');
@@ -31,7 +26,7 @@ async function register(data) {
     return fieldResponse(400, 'Password is required');
   }
 
-  let sql = `INSERT INTO USERS SET ?`;
+  let sql = `INSERT INTO users SET ?`;
   data.user_password = bcrypt.hashSync(data.user_password, 10); //password is hashed
 
   return queryResponse(sql, data).then((result) => { // <= should be an async function.
@@ -59,15 +54,6 @@ async function login(data) {
   const sql = 'SELECT * FROM users WHERE email = ?';
 
   return queryResponse(sql, data.email).then(result => {
-<<<<<<< HEAD
-    if(bcrypt.compareSync(result[0].user_password, password)) {
-      return fieldResponse(200, 'logged in.', result);
-    } else {
-      return fieldResponse(401, 'password does not match.');
-    }
-  }).catch(error => {
-      return fieldResponse(404, 'unable to login', error.sqlMessage);
-=======
       if(bcrypt.compare(data.user_password, result[0].user_password)){
         return fieldResponse(200, 'logged in.', result);
       } else {
@@ -75,7 +61,6 @@ async function login(data) {
       }
     }).catch(error => {
       return fieldResponse (404, 'unable to login', error.sqlMessage);
->>>>>>> 9d7de7dfc5e104ef1fbd037645a1c1c00b421b24
     });
 }
 
@@ -97,15 +82,9 @@ async function forgot(data) {
     if(result[0].email === data.email) {
       let token = encrypt.generateToken({email: data.email});
       let link = process.env.LINK || 'http://localhost:4200/reset-password/' + token;
-<<<<<<< HEAD
-      // sending an email to the user. 
-      mail.sendEmail(data.email, msg.resetMessage(result[0].first_name, link));
-      return fieldResponse(200, "Thank you! Please check your email we've sent you an email");
-=======
 
       mail.sendEmail(data.email, msg.resetMessage(result[0].first_name, link), msg.resetSubject());
       return fieldResponse (200, "Thank you! Please check your email we've sent you an email.");
->>>>>>> 9d7de7dfc5e104ef1fbd037645a1c1c00b421b24
     }
   }).catch(error => {
     return fieldResponse(400, 'User email does not exist, try again!', error.sqlMessage);
@@ -121,22 +100,12 @@ async function forgot(data) {
 async function reset(data) {
   let resetPass = await encrypt.verifyToken(data.token);
   let password = bcrypt.hashSync(data.user_password, 10); //password is hashed
-<<<<<<< HEAD
-=======
-
->>>>>>> 9d7de7dfc5e104ef1fbd037645a1c1c00b421b24
-  let sql = `UPDATE Users SET user_password = ? WHERE email = '${resetPass.email}'`;
+  let sql = `UPDATE users SET user_password = ? WHERE email = '${resetPass.email}'`;
 
   return queryResponse(sql, password).then(result => {
-<<<<<<< HEAD
-    return fieldResponse(200, 'successfully updated.');
-  }).catch(error => {
-    return fieldResponse(400, 'unsuccessful', error.sqlMessage);
-=======
     return fieldResponse(200, 'Successfully updated.');
   }).catch(error => {
     return fieldResponse(400, 'Unsuccessful', error.sqlMessage);
->>>>>>> 9d7de7dfc5e104ef1fbd037645a1c1c00b421b24
   });
 }
 
