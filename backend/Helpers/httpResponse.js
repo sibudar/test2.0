dbbc96cbf60ee8d1a47827eb148495de1c56ccd8
Gift <gift@thedigitalacademy.co.za>
2020-02-25 10:07:22 +0200

    added some error handling

---
+++ backend/Helpers/httpResponse.js
@@ -0,0 +1,13 @@
+/**
+ * A response.
+ * @param {*} status 
+ * @param {*} message 
+ * @param {*} data
+ * @returns status, message, data. 
+ */
+function response(status, message, data=[])
+{
+    return { status, message, data}
+}
+
+module.exports = response;
