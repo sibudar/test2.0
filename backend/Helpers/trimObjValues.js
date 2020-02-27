ce4244d76967489e54f7df16798678762f8b17d7
Gift <gift@thedigitalacademy.co.za>
2020-02-27 16:33:09 +0200

    email link

---
+++ backend/Helpers/trimObjValues.js
@@ -0,0 +1,36 @@
+/** 
+ * This function receives an object then trims all of the object's properties/values 
+ * and return the trimmed object
+ * @param {object} object
+ * @return {object}
+ */
+function trimObjValues (obj) {
+  return Object.keys (obj).reduce ((acc, curr) => {
+    acc[curr] = obj[curr].trim ();
+    return acc;
+  }, {});
+}
+
+/** 
+ * This function receives an object and that object's key then convert the object's 
+ * properties/values to lovercase and return the lowercase object's properties/values
+ * @param {object} object
+ * @param {any} key
+ * @return {object}
+ */
+function lowerObjValue (obj, key) {
+  obj['' + key] = obj['' + key].toLowerCase ();
+  return obj;
+}
+
+/** 
+ * This function receives a string and trims the whitespace on at the start of the 
+ * string and at the end of the string then return the trimmed string
+ * @param {string} string
+ * @return {string} 
+ */
+function trimString (string) {
+  return string.trim ();
+}
+
+module.exports = {trimObjValues, lowerObjValue, trimString};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-const http = require('http');
-const app = require('./app');
-const server = http.createServer(app);
-const port = process.env.PORT || 5000
+const http = require ('http');
+const app = require ('./app');
+const server = http.createServer (app);
+const port = process.env.PORT || 5000;
 
-const db = require('./config/db');
+const db = require ('./config/db');
 
-server.listen(port, ()=>{
-    console.log('Server is listening on port ' + port);
-})
+server.listen (port, () => {
+  console.log ('Server is listening on port ' + port);
+});
+
