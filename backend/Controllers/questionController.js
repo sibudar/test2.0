b9d69f612a3d4a455e47e55bfdb58e9b9657815c
Samkelisiwe Samantha Mcholo <academy_intern@academy-interns-MacBook-Air.local>
2020-03-05 15:09:00 +0200

    fixed swagger and able to add answers

--- backend/Controllers/questionController.js
+++ backend/Controllers/questionController.js
@@ -1,5 +1,6 @@
 const fieldResponse = require ('../Helpers/httpResponse');
 const queryResponse = require ('../Helpers/queryFunction');
+const fieldValidator = require ('../Helpers/validate');
 
 /**
  * Gets questions from DB.
@@ -16,4 +17,62 @@ async function getQuestions(){
     })
 }
 
-module.exports = {getQuestions};
+/**
+ * stores answers
+ * @param {*} data   
+ * data is a body the user inserts.
+ * @returns a queryResponse.
+ */
+async function addAnswer(data) {
+   
+    if(fieldValidator.validate(data.user_answer)){
+        return fieldResponse(400,"An answer to question is required", data);
+    }
+    if(fieldValidator.validate(data.id_user)){
+        return fieldResponse(400,"user id is required");
+    }
+    if(fieldValidator.validate(data.id_bus)){
+        return fieldResponse (400,"business id is required");
+    }
+
+    if(fieldValidator.validate(data.id_que)){
+        return fieldResponse (400," question id  is required");
+    }
+
+    let sql = 'CALL postAnswers(?)'
+    
+    return queryResponse(sql, [data.user_answer, data.id_user,data.id_que, data.id_bus, ]).then((result) => {
+        return fieldResponse(201,'Thank you for answering');
+    }).catch(error => {
+        return fieldResponse(500, 'Oops! we\'re experiencing some problems on our servers, please try again later!', error.sqlMessage );
+    });
+}
+
+/**
+ * stores answers
+ * @param {*} data   
+ * data is a body the user inserts.
+ * @returns a queryResponse.
+ */
+async function modifyAnswer(data) {
+   
+    if(fieldValidator.validate(data.answer_user)){
+        return fieldResponse(400,"A modified answer is required", data);
+    }
+    if(fieldValidator.validate(data.b_id)){
+        return fieldResponse(400,"business id is required");
+    }
+    if(fieldValidator.validate(data.u_id)){
+        return fieldResponse (400,"user id is required");
+    }
+
+    let sql = 'CALL updateAnswer(?)'
+    
+    return queryResponse(sql, [data.answer_user, data.b_id, data.u_id]).then((result) => {
+        return fieldResponse(201,'Thank you for updating your answer');
+    }).catch(error => {
+        return fieldResponse(500, 'Oops! we\'re experiencing some problems on our servers, please try again later!', error.sqlMessage );
+    });
+}
+
+module.exports = { getQuestions, addAnswer , modifyAnswer };
--- backend/Routes/questionRoute.js
+++ backend/Routes/questionRoute.js
@@ -7,7 +7,7 @@ const questionController = require("../Controllers/questionController");
  * /questions:
  *  get:  
  *     tags:
- *      - user
+ *      - questions
  *     summary: Get questions
  *     description: retrieve questions from th db
  *     consumes:
@@ -21,9 +21,92 @@ const questionController = require("../Controllers/questionController");
  *         description: Could not get questions requested.
  * 
  */
-userRouter.get("/questions", async(req, res) => {
+userRouter.get("/", async(req, res) => {
     result = await questionController.getQuestions();
   
+    res.status(result.status).send(result);
+  });
+
+
+  
+// [post] route to add a business to .
+/**
+ * @swagger
+ * /questions/answers:
+ *  post:
+ *     tags:
+ *      - answers
+ *     summary: Adds a an answer 
+ *     description: This can only be done by users who have business ideas
+ *     consumes:
+ *        - application/json
+ *     produces:
+ *        - application/json
+ *     parameters:
+ *        - name: Answers
+ *          description: Created answer object
+ *          in: body
+ *          required: true 
+ *          schema: 
+ *           type: object
+ *           properties:
+ *            user_answer:         
+ *              type: string
+ *            id_user:         
+ *              type: number
+ *            id_bus:         
+ *              type: number
+ *            id_que:         
+ *               type: number
+ *     responses:
+ *        200:
+ *         description: Thank you for answering.
+ *        500:
+ *         description: Oops! we\'re experiencing some problems on our servers, please try again later!.
+ */
+  userRouter.post("/answers", async(req, res) => {
+    result = await questionController.addAnswer(req.body);
+  
+    res.status(result.status).send(result);
+  });
+  
+// [patch] route to update your answer .
+/**
+ * @swagger
+ * /questions:
+ *  patch:
+ *     tags:
+ *      - answers
+ *     summary: Updates the answer
+ *     description: This can only be done by users who have answers to questions
+ *     consumes:
+ *        - application/json
+ *     produces:
+ *        - application/json
+ *     parameters:
+ *        - name: Answers
+ *          description: Created answer object
+ *          in: body
+ *          required: true 
+ *          schema: 
+ *           type: object
+ *           properties:
+ *            answer_user:         
+ *              type: string
+ *            b_id:         
+ *              type: number
+ *            u_id:         
+ *              type: number
+ *            
+ *     responses:
+ *        200:
+ *         description: Thank you for updating your answer.
+ *        500:
+ *         description: Oops! we\'re experiencing some problems on our servers, please try again later!.
+ */
+  userRouter.patch("/",async(req,res) => {
+    result = await questionController.modifyAnswer(req.body);
+
     res.status(result.status).send(result);
   });
 
