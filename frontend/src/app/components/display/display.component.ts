2eaa4e3bca8a619169e1ae0f107355fff59d4a08
chamano <ramashia@thedigitalacademy.co.za>
2020-03-13 16:04:27 +0200

    evaluation

--- frontend/src/app/components/display/display.component.ts
+++ frontend/src/app/components/display/display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-display',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+
+
+  }
+
 
   ngOnInit() {
   }
+
+ 
 
 }
