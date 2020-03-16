2de8bace74bbd3a665f2f9bc43ce21b07efaa16b
chamano <ramashia@thedigitalacademy.co.za>
2020-03-16 08:35:01 +0200

    resolved errors

--- frontend/src/app/components/display/display.component.ts
+++ frontend/src/app/components/display/display.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
-<<<<<<< HEAD
 import { ClientService } from 'src/app/services/client.service';
-=======
 import {MatDialog, MatDialogConfig} from "@angular/material";
 import { DialogComponent } from '../dialog/dialog.component';
->>>>>>> fd52cb4751a02891a69c1252d4eb0c31fc880ab3
 
 @Component({
   selector: 'app-display',
@@ -18,16 +15,13 @@ export class DisplayComponent implements OnInit {
   found = false;
   ideas : any ;
 
-<<<<<<< HEAD
-  constructor( private clientService:ClientService) {
+  constructor( private clientService:ClientService,private dialog: MatDialog) { 
 
     this.userData=localStorage.getItem('user');
     this.user = JSON.parse(this.userData);
     this.getUserIdeas();
-=======
-  constructor(private dialog: MatDialog) {}
->>>>>>> fd52cb4751a02891a69c1252d4eb0c31fc880ab3
-
+  
+    }
 
   ngOnInit() {
   }
@@ -49,19 +43,7 @@ export class DisplayComponent implements OnInit {
     
     const dialogRef = this.dialog.open(DialogComponent, dialogConfig);
 
-  getUserIdeas()
-  {
-    console.log('found')
-    var userData1 = JSON.parse(this.userData);
-    this.clientService.getIdeas(this.user.id)
-    .subscribe((data)=>{
-      console.log(data);
-      this.ideas = data;
-      this.found = true;
-      console.log(data);
-    });
 
- }
 
  
     dialogRef.afterClosed().subscribe(
@@ -72,4 +54,22 @@ export class DisplayComponent implements OnInit {
       }
   ); 
 
-}}
+  
+
+}
+
+getUserIdeas()
+{
+  console.log('found')
+  var userData1 = JSON.parse(this.userData);
+  this.clientService.getIdeas(this.user.id)
+  .subscribe((data)=>{
+    console.log(data);
+    this.ideas = data;
+    this.found = true;
+    console.log(data);
+  });
+
+}
+
+}
