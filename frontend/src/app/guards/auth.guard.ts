ebb3d45e55bade0a4e314e522e104f802f207394
Nomaseko <nomaseko@thedigitalacademy.co.za>
2020-03-24 12:06:34 +0200

    removing the interceptor

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ToasterService } from '../services/toaster.service';
-import { TokenService } from '../services/token.service';
+
 
 
 @Injectable({
@@ -10,15 +10,11 @@ import { TokenService } from '../services/token.service';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router:Router , private toast : ToasterService,private token : TokenService){}
+  constructor(private router:Router , private toast : ToasterService){}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-      if(this.token.getToken()) {
-        return true;
-      }
 
       this.toast.error('Please log in first ' , ' ' ,10000);
       return  this.router.parseUrl('/client/login');
