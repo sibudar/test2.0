fc70a3ae333e270d8666b49eb20652f4be8163fb
chamano <ramashia@thedigitalacademy.co.za>
2020-03-30 17:21:23 +0200

    digital marketing Ui

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
-const port = process.env.PORT || 5004;
+const port = process.env.PORT || 5005;
 
 const db = require('./config/db');
 
