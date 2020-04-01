d405bf7cd78d8e3c3761bd285e34bd5a70b70df5
pmahele <philani@thedigitalacademy.co.za>
2020-04-01 17:38:02 +0200

    login function from auth.service: modified.

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError } from "rxjs/operators";
 
@@ -9,26 +9,48 @@ import { map, catchError } from "rxjs/operators";
 export class AuthService {
   
   url: string = "http://localhost:5000/api/v1";
+
   constructor(private http: HttpClient) {}
 
+  /**
+   * Entering the application, we give the user a token
+   * and store the token.
+   * @param user
+   */
   login(user: any): Observable<any> {
-    return this.http.post(this.url + '/users/login', user).pipe(map(result => {
-      console.log(result);
-      sessionStorage.setItem("access_token", JSON.stringify(result));
-      return user;
-    }), catchError(error => {
-      return throwError('Something went wrong!');
-    }));
+    return this.http.post(this.url + "/users/login", user).pipe(
+      map((result: any) => {
+        sessionStorage.setItem("access_token", JSON.stringify(result.data));
+      }),
+      catchError(error => {
+        return throwError(error);
+      })
+    );
   }
 
-  verifyToken(token) {
-    return this.http.get(this.url + "/users/me", token);
+  /**
+   * Verifying a token by get request to the server.
+   * @param token
+   * @returns a user's details.
+   */
+  verifyToken(data) {
+    const headers = new HttpHeaders({ "x-access-token": data });
+    
+    return this.http.get(this.url + "/users/me", { headers: headers }).subscribe(data => {});
   }
 
+  /**
+   * Exit the application, we remove the token from our storage.
+   */
   logout() {
     sessionStorage.removeItem("access_token");
   }
 
+  /**
+   * Verification, to check if our user is logged in to the
+   * application by:
+   * @returns boolean if the token is in our storage.
+   */
   public get loggedIn(): boolean {
     return sessionStorage.getItem("access_token") !== null;
   }
