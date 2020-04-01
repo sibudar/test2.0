018d3acd29cf6c2328beb146948a8963255f9073
pmahele <philani@thedigitalacademy.co.za>
2020-04-01 17:33:55 +0200

    verify a user, sessionStorage.

--- frontend/src/app/components/display/display.component.ts
+++ frontend/src/app/components/display/display.component.ts
@@ -7,114 +7,140 @@ import { UserResponse } from 'src/app/models/user';
 import { QuestionComponent } from '../question/question.component';
 import { QuestionsResponse } from 'src/app/models/questions';
 import { ContentResponse } from 'src/app/models/content';
+import { AuthService } from 'src/app/services/auth.service';
 
 
 @Component({
-  selector: 'app-display',
-  templateUrl: './display.component.html',
-  styleUrls: ['./display.component.scss']
+  selector: "app-display",
+  templateUrl: "./display.component.html",
+  styleUrls: ["./display.component.scss"]
 })
 export class DisplayComponent implements OnInit {
-  userData: string;
-  user: any ;
+  user: any;
+  verified: any;
   found = false;
-  ideas : any ;
-  businessIdea : string;
-  description : string;
-  questions:any;
-  id_cat:any;
-  content:any;
-  contentShow: any ;
-  show = false ;
-  
-  
-  
-  constructor( private clientService:ClientService, private dialog: MatDialog) { 
-    this.userData=sessionStorage.getItem('user');
-    this.user = JSON.parse(this.userData);
+  ideas: any;
+  businessIdea: string;
+  description: string;
+  questions: any;
+  id_cat: any;
+  content: any;
+  contentShow: any;
+  show = false;
+
+  constructor(private clientService: ClientService, private auth: AuthService, private dialog: MatDialog) {
+    this.user = this.verifiedUser();
     this.getUserIdeas();
     this.Getquestions(this.id_cat);
   }
 
   ngOnInit() {
   }
-  
+
+  /**
+   * Opens a pop.
+   */
   openDialog() {
+    this.user = this.verifiedUser();
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
 
     dialogConfig.data = {
       id: 1,
-      title: 'Angular For Beginners'  
+      title: "Angular For Beginners"
     };
 
     this.dialog.open(DialogComponent, dialogConfig);
-    
+
     const dialogRef = this.dialog.open(DialogComponent, dialogConfig);
-    
-    dialogRef.afterClosed().subscribe(
-      data => {
-        if(data != undefined ) { 
-          data.id_user = this.user.id;
-          this.inserIdea(data);
-        }
-        this.dialog.closeAll()
-      });
-    }
-    
-    openQuestionDialog(id) {
-      const dialogConfig = new MatDialogConfig();
 
-      dialogConfig.disableClose = true;
-      dialogConfig.autoFocus = true;
-      dialogConfig.data = {
-        id: 1,
-        title: 'Angular For Beginners'
-      };
-
-      this.dialog.open(QuestionComponent, dialogConfig);
-  
-      const dialogRef = this.dialog.open(QuestionComponent, dialogConfig);
-
-      dialogRef.componentInstance.userID = this.user.id ; 
-      dialogRef.componentInstance.busID =id ;
-
-      dialogRef.afterClosed().subscribe(data => {
-      // if(data != undefined ){ 
-      //    data.id_user = this.user.id;
-      //     this.inserIdea(data);
-      // }
-      this.dialog.closeAll()
-    }); 
+    dialogRef.afterClosed().subscribe(data => {
+      if (data != undefined) {
+        console.log(data, 'data');
+        console.log(this.user);
+        data.id_user = this.user.id;
+        this.inserIdea(data);
+      }
+      this.dialog.closeAll();
+    });
   }
-  
+
+  /**
+   * On the pop, there's a form with question for 
+   * the user to evaluate their idea.
+   * @param id which question id is requested.
+   */
+  openQuestionDialog(id) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = {
+      id: 1,
+      title: "Angular For Beginners"
+    };
+
+    this.dialog.open(QuestionComponent, dialogConfig);
+
+    const dialogRef = this.dialog.open(QuestionComponent, dialogConfig);
+
+    dialogRef.componentInstance.userID = this.user.id;
+
+    dialogRef.componentInstance.busID = id;
+
+    dialogRef.afterClosed().subscribe(data => {
+      // if(data != undefined ){
+      //   data.id_user = this.user.id;
+      //   this.inserIdea(data);
+      // }
+      this.dialog.closeAll();
+    });
+  }
+
+  /**
+   * Adds a user's idea.
+   * @param idea inserted by the user.
+   */
   inserIdea(idea) {
-    this.clientService.insertBusinessIdea(idea).subscribe((data:UserResponse) =>{
-   // this.ideas = data.data;
-    this.getUserIdeas()
-    console.log(data);
-  });
-}
+    this.clientService.insertBusinessIdea(idea).subscribe((data: UserResponse) => {
+      console.log(data)
+        // this.ideas = data.data;
+        this.getUserIdeas();
+        console.log(data);
+      });
+  }
 
- getUserIdeas() {
-   this.clientService.getIdeas(this.user.id).subscribe((data)=> {
-    console.log(data);
-     this.ideas = data;
-     if(data.data.length > 0) {
-      this.found = true;
+  /**
+   * Gets all the user's ideas that were added by them.
+   */
+  getUserIdeas() {
+    this.clientService.getIdeas(this.user.id).subscribe(data => {
+      this.ideas = data;
+      if (data.data.length > 0) {
+        this.found = true;
+      }
+      console.log(data);
+    });
+  }
+
+  /**
+   * Gets questions from the server's endpoint base on the id.
+   * @param id_cat which question id is requested.
+   */
+  Getquestions(id_cat) {
+    this.clientService.getQuestions(2).subscribe((data: QuestionsResponse) => {
+      this.questions = data.data;
+      console.log(data);
+    });
+  }
+
+  verifiedUser() {
+    if(this.auth.loggedIn) {
+      this.verified = JSON.parse(sessionStorage.getItem("access_token"));
+      if(this.verified.auth) {
+        return this.auth.verifyToken(this.verified.token);
+      }
     }
-    console.log(data);
-  });
- }
-
-// get questions
-
-Getquestions(id_cat){
-  this.clientService.getQuestions(2).subscribe((data:QuestionsResponse) =>{
-    this.questions = data.data
-    console.log(data)
-  })
-  
-}
+  }
 }
