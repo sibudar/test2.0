810ff32c52cb0588d2f0402716bfadbc1b4b1259
sam <sam@thedigitalacademy.co.za>
2020-04-02 16:58:27 +0200

    getting an empty array on get questions test

--- backend/Tests/content.spec.js
+++ backend/Tests/content.spec.js
@@ -1,15 +1,19 @@
-// const supertest = require('supertest'); //requires supertest
-// const app = require('../app') //requires app
+const supertest = require('supertest'); //requires supertest
+const app = require('../app') //requires app
 
-// describe("Testing content endpoints", () => {
-//     it('tests the get content and returns a success message', async() => {
-//         const response = await supertest(app).get('/content').send({
-//             id_cat:'1'
-//     })
-//     expect(response.status).toBe(200)
-//     });
-   // its working now u need to make them pass now
-   // i 
- // ok let me first understand the code of i'm a bit confused
- // okay but basically ur results when make a request are in text 
-// })
+describe("Testing content endpoints", () => {
+  
+  //get content
+    it('tests the get content and returns a success message', async() => {
+        let response;
+        await supertest(app).get('/api/v1/content/{id}').send({
+            id_cat:'1'
+    }).then(res => {
+      response = JSON.parse(res.text);
+      console.log(response)
+    })
+    expect(response.message).toBe('Here is the content you requested.')
+    expect(response.status).toBe(200)
+    });
+   
+})
--- backend/Tests/idea.spec.js
+++ backend/Tests/idea.spec.js
@@ -1,25 +1,26 @@
-//const supertest = require('supertest'); //requires supertest
-//const app = require('../app');
+const supertest = require('supertest'); //requires supertest
+const app = require('../app');
 
-//describe("Testing ideas endpoints", () => {
+describe("Testing ideas endpoints", () => {
     
-    //add business idea
-    // it('tests add business idea endpoint and returns a success message', async() => {
-    //     let response ;
-    //      await supertest(app).post('/api/v1/ideas').send({
-    //         busin_idea:'the nail guru',
-    //         descript:'the nail technication',
-    //         id_user:'1'
+   // add business idea
+it('tests add business idea endpoint and returns a success message', async() => {
+        let response ;
+         await supertest(app).post('/api/v1/ideas').send({
+            busin_idea:'the nail guru',
+            descript:'the nail technication',
+                id_user:'1'
 
-    //     }).then(res => {
-    //         response = JSON.parse(res.text);
-    //         console.log(response)
-    //     })
-    //     expect(response.message).toBe('you have successfully added an idea')
-    //     expect(response.status).toBe(201);
-    // })
+        }).then(res => {
+            response = JSON.parse(res.text);
+            console.log(response)
+        })
+        expect(response.message).toBe('you have successfully added an idea')
+        expect(response.status).toBe(201);
+    })
+    
 
-//     //gets business idea
+     //gets business idea
     it('tests get business idea endpoint and returns a success message', async() => {
         let response ;
         await supertest(app).get('/api/v1/ideas/{id}').send({
@@ -33,16 +34,16 @@
     })
 
     //updates a business idea
-//     it('tests update business idea endpoint and returns a success message', async() => {
-//         let response ;
-//          await supertest(app).patch('/api/v1/ideas').send({
-//             b_id:1,
-//             u_id:1
-//         }).then(res => {
-//             response = JSON.parse(res.text)
-//             console.log(response);
-//         })
-//         expect(response.message).toBe('You have successfully deleted your idea')
-//         expect(response.status).toBe(201)
-//     })
-//  })
+    it('tests update business idea endpoint and returns a success message', async() => {
+        let response ;
+         await supertest(app).patch('/api/v1/ideas').send({
+            b_id:1,
+            u_id:1
+        }).then(res => {
+            response = JSON.parse(res.text)
+            console.log(response);
+        })
+        expect(response.message).toBe('You have successfully deleted your idea')
+        expect(response.status).toBe(201)
+    })
+ })
