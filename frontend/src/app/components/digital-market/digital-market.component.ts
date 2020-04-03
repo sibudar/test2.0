19a738c87a18041139981b60576ada73d63ceb49
Nomaseko <nomaseko@thedigitalacademy.co.za>
2020-04-03 12:11:19 +0200

    fixing ui conflicts

--- frontend/src/app/components/digital-market/digital-market.component.ts
+++ frontend/src/app/components/digital-market/digital-market.component.ts
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import { ClientService } from 'src/app/services/client.service';
-import {ContentResponse, Content } from 'src/app/models/content';
-
-@Component({
-  selector: 'app-digital-market',
-  templateUrl: './digital-market.component.html',
-  styleUrls: ['./digital-market.component.scss']
-})
-export class DigitalMarketComponent implements OnInit {
-  //content: Content[];
-  contentShow: any;
-
-  constructor(private clientService:ClientService) {
-    this.getContent(4);
-   }
-
-  ngOnInit() {
-  }
-
-getContent(id_cat) :void {
-  //console.log("the dm content works");
-  this.clientService.getContent(4).subscribe((data:ContentResponse) =>{
-    this.contentShow = data.data;
-    console.log(data);
-  })
-}
-
-=======
 import { Component, OnInit } from "@angular/core";
 import { ClientService } from "src/app/services/client.service";
 import { ContentResponse, Content } from "src/app/models/content";
@@ -67,5 +37,4 @@ export class DigitalMarketComponent implements OnInit {
       console.log(res);
     });
   }
->>>>>>> uiDesignGift
 }
