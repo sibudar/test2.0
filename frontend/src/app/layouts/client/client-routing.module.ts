392b390416d22fac31386776d701832ee897c290
pmahele <philani@thedigitalacademy.co.za>
2020-04-08 15:40:59 +0200

    added a route to dashboard.

--- frontend/src/app/layouts/client/client-routing.module.ts
+++ frontend/src/app/layouts/client/client-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ClientComponent } from './client.component';
 import { RegisterComponent } from 'src/app/components/register/register.component';
 import { LoginComponent } from 'src/app/components/login/login.component';
 import { DisplayComponent } from 'src/app/components/display/display.component';
@@ -9,23 +8,21 @@ import { LandingComponent } from 'src/app/components/landing/landing.component';
 import { ForgotPasswordComponent } from 'src/app/components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from 'src/app/components/reset-password/reset-password.component';
 
-import { AuthGuard } from 'src/app/guards/auth.guard';
 import { QuestionComponent } from 'src/app/components/question/question.component';
-import { DigitalMarketComponent } from 'src/app/components/digital-market/digital-market.component';
+import { DashboardComponent } from "src/app/components/dashboard/dashboard.component";
 
 
 const routes: Routes = [
-  { path: '', component: LandingComponent },
-  { path: '', component: LandingComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent},
-  { path: 'display', component: DisplayComponent },
-  { path: 'home', component: LandingComponent},
-  { path: 'forgotPassword', component: ForgotPasswordComponent },
-  { path: 'resetPassword/:token', component: ResetPasswordComponent },
-  { path: 'question', component: QuestionComponent }
-  
-
+  { path: "", component: LandingComponent },
+  { path: "", component: LandingComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  { path: "display", component: DisplayComponent },
+  { path: "home", component: LandingComponent },
+  { path: "forgotPassword", component: ForgotPasswordComponent },
+  { path: "resetPassword/:token", component: ResetPasswordComponent },
+  { path: "question", component: QuestionComponent },
+  { path: "dashboard", component: DashboardComponent }
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
