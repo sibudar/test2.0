50b9ef5d5a837c836d41ebd622338162172fd2c5
pmahele <philani@thedigitalacademy.co.za>
2020-04-17 13:37:28 +0200

    clean up, modify, added functions.

--- frontend/src/app/components/finance/finance.component.ts
+++ frontend/src/app/components/finance/finance.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from "@angular/core";
 import { ClientService } from "../../services/client.service";
 import { QuestionsResponse, Questions } from "src/app/models/questions";
 import { ContentResponse, Content } from "src/app/models/content";
+import { JoyrideService } from "ngx-joyride";
 
 @Component({
   selector: "app-finance",
   templateUrl: "./finance.component.html",
-  styleUrls: ["./finance.component.scss"]
+  styleUrls: ["./finance.component.scss"],
 })
+
 export class FinanceComponent implements OnInit {
   questions: Questions[];
   content: Content[];
@@ -18,23 +20,31 @@ export class FinanceComponent implements OnInit {
   pageLoaded: boolean = false;
   done: boolean = false;
 
-  constructor(private clientService: ClientService) {
-    this.Getquestions(3);
-    this.Getcontent(3);
+  constructor(private clientService: ClientService, private joyride: JoyrideService) {
+    this.getQuestions(3);
+    this.getContent(3);
   }
 
   ngOnInit() {}
 
-  Getquestions(id_cat): void {
-    this.clientService.getQuestions(3).subscribe((data: QuestionsResponse) => {
+  /**
+   * Gets questions from the server's endpoint base on the id.
+   * @param id_cat which question id is requested.
+   */
+  getQuestions(id_cat): void {
+    this.clientService.getQuestions(id_cat).subscribe((data: QuestionsResponse) => {
       this.questions = data.data;
       console.log(this.questions.length);
       console.log(data);
     });
   }
 
-  Getcontent(id_cat): void {
-    this.clientService.getContent(3).subscribe((data: ContentResponse) => {
+  /**
+   * Gets content from the server's endpoint base on the id.
+   * @param id_cat which content id is requested.
+   */
+  getContent(id_cat): void {
+    this.clientService.getContent(id_cat).subscribe((data: ContentResponse) => {
       this.content = data.data;
       this.contentShow = this.content[this.index];
       this.pageLoaded = true;
@@ -57,5 +67,15 @@ export class FinanceComponent implements OnInit {
       this.button = "Head to Digital Marketing";
       this.done = true;
     }
+  }
+
+   /**
+   * Executes joyride pop-ups to explain
+   * the journey of the entreneur.
+   */
+  tour() {
+    this.joyride.startTour({
+      steps: ["financeStep", "rateStep", "addIdeaStep"],
+    });
   }
 }
