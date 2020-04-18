0b501f842825fdff9d548221066836cb9fc78f71
chamano <ramashia@thedigitalacademy.co.za>
2020-04-18 11:12:59 +0200

    running docker img and fixing the reset password

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,14 +7,15 @@ const connect = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'noveltydb',
-  password: ''
+  password: 'novelty1234',
+  port:3308
 });
 // Connect to the database.
 connect.connect(err => {
   if(err) {
-  ///   console.log("Connection to database failed.", err);
+    console.log("Connection to database failed.", err);
   } else {
-   // console.log("Connected to database successfully.");
+    console.log("Connected to database successfully.");
   }
 });
 connect.query = util.promisify(connect.query).bind(connect);
