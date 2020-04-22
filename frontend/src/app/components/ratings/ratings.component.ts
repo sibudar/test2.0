ef0d96fc41edd99e8c07c2e285be30d46dd4a967
pmahele <philani@thedigitalacademy.co.za>
2020-04-22 15:50:35 +0200

    rate implementation: business_id trouble.

--- frontend/src/app/components/ratings/ratings.component.ts
+++ frontend/src/app/components/ratings/ratings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/services/client.service';
+import { JourneyComponent } from '../journey/journey.component';
 
 @Component({
   selector: "app-ratings",
@@ -6,12 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ["./ratings.component.scss"],
 })
 export class RatingsComponent implements OnInit {
-  
-  selected = 0;
+  selected: number = 0;
   hovered = 0;
   readonly = false;
-  
-  constructor() {}
+  id_business: any;
+  descript: any;
+  user_id: any;
+
+  constructor(private clientService: ClientService, private ratings: JourneyComponent) {
+    console.log(this.selected);
+  }
 
   ngOnInit() {}
+
+  /**
+   * Updates the rating of a business idea.
+   * @param rate
+   */
+  updateRate() {
+    let data = {
+      rate: this.selected,
+      busin_id: 6,
+      id_user: this.ratings.user.data.id,
+    };
+    this.clientService.updateRatings(data).subscribe((results) => {
+      console.log(results);
+    });
+  }
 }
