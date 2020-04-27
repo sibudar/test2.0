bfd66005bef62350c21d1727dc2a615c318b61ea
Gift <giftmbanda@gmail.com>
2020-04-27 22:53:04 +0200

    connecting the domain search input box in developers with the clientService

--- frontend/src/app/components/developers/developers.component.ts
+++ frontend/src/app/components/developers/developers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+import { ClientService } from 'src/app/services/client.service';
+import { ToasterService } from 'src/app/services/toaster.service';
 
 @Component({
   selector: 'app-developers',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevelopersComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private clientService: ClientService, private formBuilder: FormBuilder, private toastr: ToasterService) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      domain: new FormControl("", [Validators.required])
+    });
+  }
+
+  postDomain(): void {
+    this.clientService.postDomain(this.form.value).subscribe((res) => {
+      this.toastr.success(res.data.domain_Availability, res.data.domain, 6000);
+      console.log(res);
+    });
   }
 
 }
