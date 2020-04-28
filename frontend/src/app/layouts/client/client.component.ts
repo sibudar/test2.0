1275413b26b7631434de4512c7c1a20f6c3692c4
pmahele <philani@thedigitalacademy.co.za>
2020-04-28 09:53:41 +0200

    nav-bar.

--- frontend/src/app/layouts/client/client.component.ts
+++ frontend/src/app/layouts/client/client.component.ts
@@ -4,38 +4,35 @@ import { UserResponse, LoginResponse } from 'src/app/models/user';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-client',
-  templateUrl: './client.component.html',
-  styleUrls: ['./client.component.scss']
+  selector: "app-client",
+  templateUrl: "./client.component.html",
+  styleUrls: ["./client.component.scss"],
 })
 export class ClientComponent implements OnInit {
-
-  loggedIn: boolean ; 
+  loggedIn: boolean;
   user: LoginResponse = {
-      id : 0,
-      first_name : '',
-      last_name : '',
-      email : '',
-      new_user: 1
+    id: 0,
+    first_name: "",
+    last_name: "",
+    email: "",
+    new_user: 1,
   };
 
-  constructor(private clientService:ClientService , private auth: AuthService ) { 
-    
-    this.loggedIn = this.auth.loggedIn ;
-  
+  constructor(private clientService: ClientService, private auth: AuthService) {
+    this.loggedIn = this.auth.loggedIn;
   }
 
   ngOnInit() {
-    
-    if( this.loggedIn ) {
+    if (this.loggedIn) {
       let verified = JSON.parse(sessionStorage.getItem("access_token"));
-        this.auth.verifyToken(verified.token ).subscribe( (res:UserResponse) => {
-          this.user = res.data;
-          console.log(this.user)
-  
-        });
+      this.auth.verifyToken(verified.token).subscribe((res: UserResponse) => {
+        this.user = res.data;
+        console.log(this.user);
+      });
     }
   }
 
-
+  logout() {
+    this.auth.logout();
+  }
 }
