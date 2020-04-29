e4a6e1cdebf8e936abbb077ad7e84159a0761905
pmahele <philani@thedigitalacademy.co.za>
2020-04-30 01:20:06 +0200

    insert key if there's none.

--- frontend/src/app/components/bridge-spinner/bridge-spinner.component.ts
+++ frontend/src/app/components/bridge-spinner/bridge-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { ClientService } from 'src/app/services/client.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router, NavigationEnd } from '@angular/router';
@@ -16,7 +16,16 @@ export class BridgeSpinnerComponent implements OnInit {
   trackLink: ArrayBuffer;
   link: string;
   checkLength: any;
-  constructor(private clientService: ClientService, private auth: AuthService,private router: Router) {
+  brideKeys: any;
+  @HostBinding("class.dKey")
+  dKey: number;
+  first: any;
+
+  constructor(
+    private clientService: ClientService,
+    private auth: AuthService,
+    private router: Router
+  ) {
     this.verifiedUser();
   }
 
@@ -29,45 +38,70 @@ export class BridgeSpinnerComponent implements OnInit {
     if (this.auth.loggedIn) {
       this.verified = JSON.parse(sessionStorage.getItem("access_token"));
       if (this.verified.auth) {
-        this.auth.verifyToken(this.verified.token).subscribe((data: LoginResponse) => {
-          this.routeLastVisited(data);
-          this.update(data.id);
-        });
+        this.auth
+          .verifyToken(this.verified.token)
+          .subscribe((data: LoginResponse) => {
+            this.routeLastVisited(data);
+            this.update(data.id);
+          });
       }
     }
   }
 
   routeLastVisited(id) {
-    if(id.data.new_user == 1) {
+    if (id.data.new_user == 1) {
       this.clientService.getLink(id.data.id).subscribe((data) => {
         this.checkLength = data;
-        if(Object.keys(this.checkLength.data).length < 1) {
-          let first = { user_id: id.data.id, link: "client/display" };
+        if (Object.keys(this.checkLength.data).length < 1) {
+          let first = { id: id.data.id, link: "client/display" };
           this.clientService.start(first).subscribe((e) => {
-            console.log(e);
+            this.first = e;
+            this.update(this.first.data[0].id);
+            console.log(this.first.data[0].id), "first start.";
+            this.router.navigate(["client/display"]);
           });
-          this.update(this.checkLength.data[0].id);
-          this.router.navigate(["client/display"]);
         } else {
           this.update(this.checkLength.data[0].id);
           this.router.navigate([this.checkLength.data[0].link]);
         }
       });
     } else {
+      this.keys(id.data.id);
       this.router.navigate(["client/dashboard"]);
     }
   }
 
   update(id) {
-    this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe((e: NavigationEnd) => {
-      this.link = e.url;
-      let data = {
-        user_id: id,
-        link: this.link,
-      };
-      this.clientService.tracking(data).subscribe((results) => {
-        console.log(results);
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        this.link = e.url;
+        let data = {
+          user_id: id,
+          link: this.link,
+        };
+        this.clientService.tracking(data).subscribe((results) => {
+          console.log(results);
+        });
       });
+  }
+
+  keys(id) {
+    this.clientService.getKey(id).subscribe((data) => {
+      this.checkLength = data;
+      if (Object.keys(this.checkLength.data).length < 1) {
+        let first = { id: id, key: 1 };
+        this.clientService.firstKey(first).subscribe((e) => {
+          this.brideKeys = e;
+          console.log(this.brideKeys.data.key, "bridge key");
+          this.dKey = this.brideKeys.data.key;
+          console.log(this.dKey, "dKey bridge");
+        });
+      }
+      // else {
+      //   console.log(this.checkLength.data.givenKeys, 'bride keys');
+      //   this.dKey = this.checkLength.data.givenKeys;
+      // }
     });
   }
 }
