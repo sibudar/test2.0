f5d95048c644207fbe76e5aa0737e83c18a37f52
pmahele <philani@thedigitalacademy.co.za>
2020-04-30 13:48:39 +0200

    clean up code, restructure..

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -14,48 +14,59 @@ export class DashboardComponent implements OnInit {
   checkLength: Object;
 
   constructor(private joyride: JoyrideService, private bridge: ClientComponent, private clientService: ClientService) {
-    this.clientService.getKey(this.bridge.user.id).subscribe((data) => {
-      this.checkLength = data;
-      console.log(this.checkLength);
-      // this.key = this.checkLength.data.givenKeys;
-    });
+    this.getKey(this.bridge.user.id);
   }
 
   ngOnInit() {
-    
   }
 
-  triggerKeys() {
+  /**
+   * Which tour can be triggered.
+   */
+  triggerKeys(key) {
     console.log(this.key, "dashboard key");
-    if(this.key == 1) {
+    if(key == 1) {
       this.firstTour();
     }
-    if(this.key == 2) {
+    if(key == 2) {
       this.secondTour();
     }
-    if(this.key == 3) {
+    if(key == 3) {
       this.thirdTour()
     }
   }
 
+  /**
+   * Tour, helps the user understand.
+   */
   firstTour() {
     this.joyride.startTour({
       steps: ["ideaStep"],
     });
   }
 
+  /**
+   * Tour, helps the user understand.
+   */
   secondTour() {
     this.joyride.startTour({
       steps: ["developerStep"],
     });
   }
 
+  /**
+   * Tour, helps the user understand.
+   */
   thirdTour() {
     this.joyride.startTour({
       steps: ["digitalMarketingStep"],
     });
   }
 
+  /**
+   * Styling for a card: ngStyle
+   * @returns an object that contains styles.
+   */
   firstStyles(): Object {
     if(this.key > 0) {
       return { opacity: "5" };
@@ -63,6 +74,10 @@ export class DashboardComponent implements OnInit {
     return { opacity: "0.1", cursor: "not-allowed" };
   }
 
+  /**
+   * Styling for a card: ngStyle
+   * @returns an object that contains styles.
+   */
   secondStyles(): Object {
     if(this.key > 1) {
       return { opacity: "5" };
@@ -70,18 +85,27 @@ export class DashboardComponent implements OnInit {
     return { opacity: "0.0", cursor: "not-allowed", "pointer-events": "none" };
   }
 
+  /**
+   * Styling for a card: ngStyle
+   * @returns an object that contains styles.
+   */
   thirdStyles(): Object {
-    if (this.key > 2) {
+    if(this.key > 2) {
       return { opacity: "5" };
     }
     return { opacity: "0.0", cursor: "not-allowed", "pointer-events": "none" };
   }
 
+  /**
+   * Gets the key the user have on the database.
+   * @param id user's id.
+   */
   getKey(id) {
     this.clientService.getKey(id).subscribe((data) => {
       this.checkLength = data;
       console.log(this.checkLength);
-      // this.key = this.checkLength.data.givenKeys;
+      // this.key = this.checkLength.data[0].givenKeys;
+      this.triggerKeys(this.key);
     });
   }
 }
