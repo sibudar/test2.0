8176c41738d7e5a3b040b11f3c25c272d18170d7
sam <sam@thedigitalacademy.co.za>
2020-05-04 10:04:50 +0200

    evaluation

--- frontend/src/app/components/question/question.component.ts
+++ frontend/src/app/components/question/question.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { QuestionsResponse, Questions } from 'src/app/models/questions';
-import { ClientService } from '../../services/client.service';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Questions, QuestionsResponse } from 'src/app/models/questions';
+import { ClientService } from '../../services/client.service';
 
 
 @Component({
@@ -13,6 +13,7 @@ import { Router } from '@angular/router';
 export class QuestionComponent implements OnInit {
 
   questions: Questions[];
+  question: Questions = {id: 0 , q_name: 'loading'} ;
   show: boolean = false;
   index: number = 0;
   button: string = "Next";
@@ -20,26 +21,61 @@ export class QuestionComponent implements OnInit {
   done: boolean = false;
   formQuestion: FormGroup;
   frmControlNames: string[] = [];
+  busID: any;
+  userID: any;
 
   constructor(private clientService: ClientService, private fb: FormBuilder, private router: Router) {
-    this.Getquestions(1);
+   this.Getquestions() ;
   }
 
   ngOnInit() {
     this.formQuestion = this.fb.group({
-
+      answer: ['', []],
     });
   }
 
-  Getquestions(id_cat): void {
+  Getquestions(): void {
     this.clientService.getQuestions(1).subscribe((data: QuestionsResponse) => {
     this.questions = data.data
-      console.log(this.questions.length)
-      console.log(data)
+    this.question = this.questions[this.index]
+    console.log(this.questions) ;
     })
   }
 
+  save() {
+    console.log(this.formQuestion.value);
+    this.answerQuestion()
+  //  if(this.busID != undefined) {
+  //     this.answerQuestion()
+  //   }
+    // this.yesClick()
+  }
+
+  answerQuestion() {
+    let idUser = this.userID;
+    let idBus = this.busID;
+    let serviceClient = this.clientService;
+    let fromQuiz: any = this.formQuestion.value
+    
+    this.questions.forEach(element => {
+      
+      let data = {
+        "user_answer": fromQuiz[element.id],
+        "id_user": idUser, 
+        "id_bus": idBus,
+        "id_que": element.id
+      }
+      let service = serviceClient.postAnswers(data);
+      service.subscribe((result) => {
+        console.log(result);
+      })
+      
+    });
+  }
+
+  
   yesClick(): void {
+
     if (this.index++ <= this.questions.length - 1) {
       this.show = false;
       this.button = "Next";
@@ -48,6 +84,9 @@ export class QuestionComponent implements OnInit {
       this.done = true;
       this.show = true;
     }
+
+    
+
   }
 
   continueToLegal() {
