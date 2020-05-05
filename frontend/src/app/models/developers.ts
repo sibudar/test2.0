cb4327f091ffb1d4672468a109ecc93e41fb5469
Nomaseko <nomaseko@thedigitalacademy.co.za>
2020-05-05 11:33:59 +0200

    deveveloper'profile is fine

--- frontend/src/app/models/developers.ts
+++ frontend/src/app/models/developers.ts
@@ -5,40 +5,40 @@ export interface DeveloperResponse {
 }
 
 export interface DeveloperProfile {
-
-  name:string;
+  name: string;
   // experience
   title: String;
   company: String;
-  yearsOfExperience:number;
+  yearsOfExperience: number;
+  workStart: String;
+  workEnd: String;
   // current: boolean;
   description: String;
-  skills:string;
-
+  // skills
+  frontendSkills: String;
+  backendSkills: String;
+  databaseSkills: String;
+  devopSkills: String;
 
   //education
   school: String;
   degree: String;
   fieldofstudy: String;
+  educationStart: String;
+  educationEnd: String;
 
   //social
   twitter: String;
   facebook: String;
-  
 }
-
 
 // let devProfile : DeveloperProfile[] =[];
 // devProfile = [{ name:'Kagiso Marabe', title:'Software Developer',company: 'Digital Academy' , yearsOfExperience:2 ,description: 'developing restful apis' , skills: 'html, css, angular,php, java, nodejs,Sql,mysql workbench' , school:'University Of Pretoria' , degree: 'Information Technology' , fieldofstudy: 'ICT' ,twitter:'link' ,facebook: 'link' },
 // { name:'Jaden Boikhutso',title:'Ms',company: 'Asolutions' , yearsOfExperience:4 ,description: 'designing responsive webistes asn applications' , skills: 'html, css, angular,php, java, nodejs,Sql,mysql workbench' , school:'University Of South Africa' , degree: 'Information Technology' , fieldofstudy: 'ICT' ,twitter:'link' ,facebook: 'link' },
-// {name:'Lesego Ntolo', title:'Mr',company: 'ABSA' , yearsOfExperience:3 ,description: 'Specialising with developing online shopping(e-commerce) webpages' , skills: 'html, css,react,php, java, nodejs,mongo' , school:'WITS' , degree: 'Information Technology' , fieldofstudy: 'ICT' ,twitter:'link' ,facebook: 'link' }, 
+// {name:'Lesego Ntolo', title:'Mr',company: 'ABSA' , yearsOfExperience:3 ,description: 'Specialising with developing online shopping(e-commerce) webpages' , skills: 'html, css,react,php, java, nodejs,mongo' , school:'WITS' , degree: 'Information Technology' , fieldofstudy: 'ICT' ,twitter:'link' ,facebook: 'link' },
 // {name:'Given Smith', title:'Mr',company: 'Digital Academy' , yearsOfExperience:5 ,description: 'developing restful apis' , skills: 'html, css, angular,php, java,end-to-end testing, nodejs,Sql,mysql workbench' , school:'Tshwane University Of Technology' , degree: 'Information Technology' , fieldofstudy: 'ICT' ,twitter:'link' ,facebook: 'link' }
 
 // ];
-
-
-
-
 
 // export interface education {
 //   school: String;
@@ -49,7 +49,6 @@ export interface DeveloperProfile {
 //   current: Date;
 //   description: String;
 // }
-
 
 // export interface social {
 //   twitter: String;
