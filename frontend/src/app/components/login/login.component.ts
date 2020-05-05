e761d60a6c63f3e15b7b5d45e281bf5f3375310a
Nomaseko <nomaseko@thedigitalacademy.co.za>
2020-05-05 16:00:36 +0200

    correcting spellings

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,19 +1,22 @@
-import { Router } from '@angular/router';
-import { ToasterService } from './../../services/toaster.service';
-import { UserResponse } from 'src/app/models/user';
-import { ClientService } from 'src/app/services/client.service';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { AuthService } from 'src/app/services/auth.service';
-import { first } from 'rxjs/operators';
-
-
+import { Router } from "@angular/router";
+import { ToasterService } from "./../../services/toaster.service";
+import { UserResponse } from "src/app/models/user";
+import { ClientService } from "src/app/services/client.service";
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
+import { AuthService } from "src/app/services/auth.service";
+import { first } from "rxjs/operators";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
   userToken: any;
@@ -31,7 +34,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.form = this.formBuilder.group({
       user_password: ["", Validators.required],
-      email: new FormControl("", [Validators.required, Validators.email])
+      email: new FormControl("", [Validators.required, Validators.email]),
     });
   }
 
@@ -41,13 +44,20 @@ export class LoginComponent implements OnInit {
    * @throws error if something went wrong.
    */
   login(): void {
-    this.auth.login(this.form.value).subscribe((data) => {
-      this.toastr.success('successfully logged in','novelty',2000);
-      this.router.navigate(['client/dashboard']); 
-    },error =>{
-      console.log(error);
-      this.toastr.error('Unable to login, please try again later' ,'novelty',10000);
-      this.isLoginFailed = true;
-    });
+    this.auth.login(this.form.value).subscribe(
+      (data) => {
+        this.toastr.success("successfully logged in", "novelty", 2000);
+        this.router.navigate(["client/display"]);
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.error(
+          "Unable to login, please try again later",
+          "novelty",
+          10000
+        );
+        this.isLoginFailed = true;
+      }
+    );
   }
 }
