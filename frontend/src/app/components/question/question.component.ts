c4673eb002e962fb650f79ba8813a5a8e940113f
pmahele <philani@thedigitalacademy.co.za>
2020-05-05 22:42:27 +0200

    answer a question.

--- frontend/src/app/components/question/question.component.ts
+++ frontend/src/app/components/question/question.component.ts
@@ -6,14 +6,13 @@ import { ClientService } from '../../services/client.service';
 
 
 @Component({
-  selector: 'app-question',
-  templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  selector: "app-question",
+  templateUrl: "./question.component.html",
+  styleUrls: ["./question.component.scss"],
 })
 export class QuestionComponent implements OnInit {
-
   questions: Questions[];
-  question: Questions = {id: 0 , q_name: 'loading'} ;
+  question: Questions = { id: 0, q_name: "loading" };
   show: boolean = false;
   index: number = 0;
   button: string = "Next";
@@ -23,69 +22,63 @@ export class QuestionComponent implements OnInit {
   frmControlNames: string[] = [];
   busID: any;
   userID: any;
+  local: any;
 
-  constructor(private clientService: ClientService, private fb: FormBuilder, private router: Router) {
-   this.Getquestions() ;
+  constructor(
+    private clientService: ClientService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.Getquestions();
   }
 
   ngOnInit() {
     this.formQuestion = this.fb.group({
-      answer: ['', []],
+      answer: ["", []],
     });
   }
 
   Getquestions(): void {
     this.clientService.getQuestions(1).subscribe((data: QuestionsResponse) => {
-    this.questions = data.data
-    // let i = 0; 
-    // this.questions.forEach(element => {
-    //   this.formQuestion.addControl(element.id, new FormControl('', Validators.required));
-    //   i++ ;
-    // });
-    this.question = this.questions[this.index]
-    console.log(this.questions) ;
-
-    
-    })
-  }
-
-  save() {
-    console.log(this.formQuestion.value);
-    this.answerQuestion()
-  //  if(this.busID != undefined) {
-  //     this.answerQuestion()
-  //   }
-     this.yesClick()
-  }
-
-  answerQuestion() {
-    
-    let local = JSON.parse(localStorage.getItem('Value'));
-    let idBus = local.id
-    let idUser = local.id_user
-    let serviceClient = this.clientService;
-    let fromQuiz: any = this.formQuestion.value
-    
-    this.questions.forEach(element => {
-      
-      let data = {
-        "user_answer": fromQuiz[element.id],
-        "id_user": idUser, 
-        "id_bus": idBus,
-        "id_que": element.id
-      }
-      let service = serviceClient.postAnswers(data);
-      service.subscribe((result) => {
-        console.log(result);
-      })
-      
+      this.questions = data.data;
+      // let i = 0;
+      // this.questions.forEach(element => {
+      //   this.formQuestion.addControl(element.id, new FormControl('', Validators.required));
+      //   i++ ;
+      // });
+      this.question = this.questions[this.index];
+      console.log(this.questions);
     });
   }
 
-  
-  yesClick(): void {
+  save() {
+    this.answerQuestion();
+    //  if(this.busID != undefined) {
+    //     this.answerQuestion()
+    //   }
+    this.yesClick();
+  }
 
-    if (this.index++ <= this.questions.length - 1) {
+  answerQuestion() {
+    this.local = JSON.parse(localStorage.getItem("Value"));
+    let idBus = this.local.id;
+    let idUser = this.local.id_user;
+    let answer = this.formQuestion.value;
+    
+    let data = {
+      user_answer: answer.answer,
+      id_user: idUser,
+      id_que: this.question.id,
+      id_bus: idBus,
+    };
+    console.log(data);
+    this.clientService.postAnswers(data).subscribe((result) => {
+      console.log(result);
+    });
+  }
+
+  yesClick(): void {
+    if(this.index++ <= this.questions.length - 1) {
       this.show = false;
       this.button = "Next";
     } else {
@@ -93,13 +86,10 @@ export class QuestionComponent implements OnInit {
       this.done = true;
       this.show = true;
     }
-
-    
-
   }
 
   continueToLegal() {
-    this.router.navigate(['client/legalJourney'])
+    this.router.navigate(["client/legalJourney"]);
   }
 }
 
