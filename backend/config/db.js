9c84282ede5d57b870133fc1f49f3f51b0f532bb
Nomaseko <nomaseko@thedigitalacademy.co.za>
2020-05-08 08:54:29 +0200

    accepting changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,24 +4,14 @@ const util = require("util");
 require("dotenv").config();
 // Create connection.
 const connect = mysql.createConnection({
-  
-    host: 'localhost',
-    user: 'root',
-<<<<<<< HEAD
-    database: 'noveltydb'
-    // password: 'novelty1234',
-    // port: 5506
-=======
-    database: 'noveltydb',
-    password: ''
-   
-  
-
->>>>>>> 760a09529ee692d6da6836aa54d65d7b5ec2d499
+  host: "localhost",
+  user: "root",
+  database: "noveltydb",
+  password: "",
 });
 // Connect to the database.
-connect.connect(err => {
-  if(err) {
+connect.connect((err) => {
+  if (err) {
     console.log("Connection to database failed.", err);
   } else {
     console.log("Connected to database successfully.");
@@ -29,4 +19,3 @@ connect.connect(err => {
 });
 connect.query = util.promisify(connect.query).bind(connect);
 module.exports = connect;
- 
